160da0cfb68754d2b36b595efd9fcaf21b6db6d5
Mary_Butsykina <mary.dovlatova16@gmail.com>
2023-11-20 12:38:54 +0700

    MqqtClient basic

--- Ethernet/MqttClient.py
+++ Ethernet/MqttClient.py
@@ -1,4 +1,5 @@
 from paho.mqtt import client as mqtt_client
+import json
 
 
 class MqttClient:
@@ -12,13 +13,22 @@ class MqttClient:
 
     def connect(self):
         def on_connect(client, userdata, flags, rc):
-            pass
-        pass
+            if not rc:
+                print("Connected to server")
+            else:
+                print('Connection failed')
+        self.client = mqtt_client.Client(self.client_id)
+        self.client.on_connect = on_connect
+        self.client.connect(self.broker, self.port)
+        return self.client
 
     def subscribe(self):
         def on_message(client, userdata, msg):
-            pass
-        pass
+            data = json.loads(str(msg.payload.decode('utf-8')))
+        self.client.subscribe(self.topic)
+        self.client.on_message = on_message
 
     def run(self):
-        pass
+        self.connect()
+        self.subscribe()
+        self.client.loop_forever()
